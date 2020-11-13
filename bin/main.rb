ca95b3a8c7ddd096a6b872d4968611c678411bdb
uchennaanya <uchennaongit@gmail.com>
2020-11-13 11:09:15 +0100

    Add an a demo array

--- bin/main.rb
+++ bin/main.rb
@@ -10,8 +10,8 @@ class UserInterface
     end
 
     def board
-    board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    board.each {|items| puts items }
+    board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    board.each {|items| p items }
     end
 
     def player
@@ -25,14 +25,24 @@ class UserInterface
         if symbol == "x" || symbol == "o"
             puts "#{player1}'s symbol: #{symbol}"
             puts "The board..."
+            
             puts "______________"
+                    board
             puts "______________"
             puts "#{player1} Playing..."
+
+           
+
             player1_move = gets.chomp
             else
             puts "invalid symbol you can only choose X or O"
             player1_move = gets.chomp
             puts "#{player1} Playing..."
+
+            puts "______________"
+                    board
+            puts "______________"
+
             choice = gets.chomp
         end
     end

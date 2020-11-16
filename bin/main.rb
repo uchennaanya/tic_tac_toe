60d61915cf915dfc60c96be6079ecaf6bb57e55a
uchennaanya <uchennaongit@gmail.com>
2020-11-16 22:51:46 +0100

    Fix linters

--- bin/main.rb
+++ bin/main.rb
@@ -1,16 +1,28 @@
 #!/usr/bin/env ruby
 
+# Your game is required to inform if the selected move is invalid (without computing what an invalid move is).
+# One way you can achieve this is by storing a particular number to a variable and if a player
+# chooses that number you can tell them that the move is invalid.
+# This is just to mock the basic flow of the game and those not require any logic.
+# Linter check is failing which means there are some lint errors, please check and correct those errors.
+# The game should inform the player if the selected move is a winning move.
+# or a draw (without computing the winning move).
+# Just like I stated above you can assign a particular number to a variable and if that number is chosen
+# you can assume that the game is won or it is a draw.
+# Once you are done with the changes, please request another review.
+# Happy coding
+# slack(@ebeagusamuel)
+
 class UserInterface
+  def initialize
+    @cells = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @moves = 0
+    @player2sym = 'o'
+    @player1sym = 'x'
+  end
 
-    def initialize
-        @cells = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        @moves = 0
-        @player_2_sym = "o"
-        @player_1_sym = "x"
-    end
-
-    def board
-        puts <<-HEREDOC
+  def board
+    puts <<-HEREDOC
 
             #{@cells[0]}  | #{@cells[1]} | #{@cells[2]}
             ---+---+---
@@ -18,53 +30,52 @@ class UserInterface
             ---+---+---
             #{@cells[6]}  | #{@cells[7]} | #{@cells[8]}
 
-        HEREDOC
+    HEREDOC
+  end
+
+  def playermoves
+    while @moves < @cells.size - 1
+
+      board
+      puts "player #1 Please select a number (0-9) to be replaced by #{@player1sym}"
+      gets.chomp
+
+      board
+      puts 'Player #2 please select a (0-9) number to be replaced by ' + @player2sym = @player1sym == 'x' ? 'o' : 'x'
+      gets.chomp
+
+      @moves += 2
     end
+    puts 'GAME OVER!'
+  end
 
-    def players
-    
-    puts "Player #1: Your name please"
-    player_1 = gets.chomp
-    
-    puts "Please choose a symbol (X or O) #{player_1}"
+  def players
+    puts 'Player #1: Your name please'
+    player1 = gets.chomp
 
-    @player_1_sym = gets.chomp
-        
-        if @player_1_sym == "x" || @player_1_sym == "o"
-           
-            puts "#{player_1}'s symbol: #{@player_1_sym}"
-            
-            puts "Player #2 your name:"
-            @player_2 = gets.chomp
+    puts "Please choose a symbol (X or O) #{player1}"
 
-            puts @player_1_sym == "x" ? player_2 = @player_2 + "'s symbol: o" : player_2 = @player_2 + "'s symbol: x"
-            
-            while @moves < @cells.size-1 do
-                
-            board
-            puts "player #1 Please select a number (0-9) to be replaced by #{@player_1_sym}"
-                player_1_move = gets.chomp
+    @player1sym = gets.chomp
 
-            board
-            
-            puts "Player #2 please select a (0-9) number to be replaced by #{@player_1_sym == 'x' ? @player_2_sym = 'o' : @player_2_sym = 'x'} "
-                player_2_move = gets.chomp
+    if @player1sym == 'x' || @player1sym == 'o'
 
-            @moves += 2
-            end
-            puts "GAME OVER!"
-        else
-            puts "invalid symbol you can only choose X or O"
+      puts "#{player1}'s symbol: #{@player1sym}"
 
-            puts "#{player_1}'s symbol: #{@player_1_sym}"
-            puts "Player #2 your name:"
-            player_2 = gets.chomp
+      puts 'Player #2 your name:'
+      player2 = gets.chomp
 
-            puts "#{player_1} Please select any available number to be replace by #{player_1_sym}"
+      puts @player1sym == 'x' ? "#{player2}'s symbol: o" : "#{player2}'s symbol: x"
+      playermoves
+    else
+      puts 'invalid symbol you can only choose X or O'
+      @player1sym = gets.chomp
+      puts "#{player1}'s symbol: #{@player1sym}"
 
-            board
-        end
+      puts 'Player #2 your name:'
+      gets.chomp
     end
+    playermoves
+  end
 end
 
 userinterface = UserInterface.new

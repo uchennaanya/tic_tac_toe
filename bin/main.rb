5989e5801af23cf454126411ce6b7cbae6b7ff5b
uchennaanya <uchennaongit@gmail.com>
2020-11-23 07:19:33 +0100

    Fix game flow

--- bin/main.rb
+++ bin/main.rb
@@ -1,85 +1,132 @@
 #!/usr/bin/env ruby
-# rubocop:disable Metrics/CyclomaticComplexity
-
-class UserInterface
+class Interface
+  attr_accessor :active_player
   def initialize
+    @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     @player1 = nil
     @player2 = nil
-    @cells = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    @player2sym = 'o'
-    @player1sym = 'x'
-    @winingmove = 5
-    @invalidmove = 3
-    @draw = 8
   end
 
-  def board
+  # METHODS
+  def display_board(_board)
     puts <<-HEREDOC
-
-            #{@cells[0]}  | #{@cells[1]} | #{@cells[2]}
-            ---+---+---
-            #{@cells[3]}  | #{@cells[4]} | #{@cells[5]}
-            ---+---+---
-            #{@cells[6]}  | #{@cells[7]} | #{@cells[8]}
-
+      #{@board[0]} | #{@board[1]} | #{@board[2]}
+      -----------
+      #{@board[3]} | #{@board[4]} | #{@board[5]}
+      -----------
+      #{@board[6]} | #{@board[7]} | #{@board[8]}
     HEREDOC
   end
 
-  def playermoves
-    # currentplayer = @player1
-    # currentplayersymbol = @player1sym
-    playerchoice = true
+  def draw?(board)
+    board.all? { |i| i.is_a?(String) }
+  end
 
-    while playerchoice
-      board
-      puts "#{currentplayer} Please select a number (0-9) to be replaced by #{currentplayersymbol}"
-      move = gets.chomp.to_i
-      if move == @winingmove
-        puts "#{currentplayer} is the Winner"
-        playerchoice = false
-      elsif move == @draw
-        puts 'The game ends in draw!'
-        playerchoice = false
-      elsif move == @invalidmove
-        puts 'Invalid move!'
-      else
-        currentplayer = currentplayer == @player1 ? @player2 : @player1
-        currentplayersymbol == @player1sym ? currentplayersymbol = @player2sym : currentplayer = @player1sym
-      end
+  def win?(board)
+    combinations = [
+      [board[0], board[1], board[2]],
+      [board[3], board[4], board[5]],
+      [board[6], board[7], board[8]],
+      [board[0], board[3], board[6]],
+      [board[1], board[4], board[7]],
+      [board[2], board[5], board[8]],
+      [board[0], board[4], board[8]],
+      [board[2], board[4], board[6]]
+    ]
+    combinations.any? do |combo|
+      combo.all? { |i| i == 'X' } || combo.all? { |i| i == 'O' }
     end
   end
 
-  def players
-    puts 'Player #1: Your name please'
-    @player1 = gets.chomp
+  def welcome_title
+    puts <<-HEREDOC
+      -------------How to Play------------------------
+      The 1st player is represented by X, the 2nd player is O.
+      Each chooses numbers from 1 to 9 to select desired cell
+      ------------------------------------------------
+    HEREDOC
+  end
 
-    puts "Please choose a symbol (X or O) #{@player1}"
+  def validate_name(name)
+    loop do
+      break unless name.strip == ''
 
-    @player1sym = gets.chomp
-
-    if @player1sym == 'x' || @player1sym == 'o'
-
-      puts "#{@player1}'s symbol: #{@player1sym}"
-
-      puts 'Player #2 your name:'
-      @player2 = gets.chomp
-
-      puts @player1sym == 'x' ? "#{@player2}'s symbol: o" : "#{@player2}'s symbol: x"
-      playermoves
-    else
-      puts 'invalid symbol you can only choose X or O'
-      @player1sym = gets.chomp
-      puts "#{@player1}'s symbol: #{@player1sym}"
-
-      puts 'Player #2 your name:'
-      gets.chomp
-
+      print 'Please enter a name: '
+      name = gets.chomp
     end
-    playermoves
+    name
+  end
+
+  def player_name
+    puts 'Player 1 please your name:'
+    @player1 = gets.chomp
+    @player1 = validate_name(@player1)
+    puts 'Player 2 please your name:'
+    @player2 = gets.chomp
+    @player2 = validate_name(@player2)
+    [@player1, @player2]
+  end
+
+  def validate_position(position, board)
+    loop do
+      break if (1..9).include?(position) && !board[position - 1].is_a?(String)
+
+      puts 'Please enter a valid number from 1 to 9 to be replaced by your symbol' unless (1..9).include?(position)
+      puts 'Position already taken , Please choose another: '
+      position = gets.chomp.to_i if board[position - 1].is_a?(String)
+    end
+    position
+  end
+
+  def info
+    welcome_title
+
+    puts 'Enter any key to start or C to cancel.'
+    answer = gets.chomp.downcase
+    abort if answer == 'c'
+
+    @player1, @player2 = player_name
+
+    puts "#{@player1} will be using 'X', while #{@player2} will be using 'O'"
+  end
+
+  def finished
+    display_board(@board)
+    active_player == @player1 ? @player2 : @player1
+  end
+
+  def play
+    info
+
+    display_board(@board)
+    active_player = @player1
+
+    loop do
+      puts "#{active_player}: Choose a poistion"
+
+      position = gets.chomp.to_i
+      position = validate_position(position, @board)
+
+      # Update board with current player's symbol
+      @board[position - 1] = active_player == @player1 ? 'X' : 'O'
+
+      # Stop if
+      if win?(@board)
+        display_board(@board)
+        puts 'Congratulations!'
+        puts "#{active_player} is the winner!"
+        break
+      elsif draw?(@board)
+        display_board(@board)
+        puts 'the game is a tie, try again'
+        break
+      end
+
+      # Show updated board
+      finished
+    end
   end
 end
 
-userinterface = UserInterface.new
-userinterface.players
-
-# rubocop:enable Metrics/CyclomaticComplexity
+interface = Interface.new
+interface.play
